f217b899ae67d1d64bd3c8ec9d0cdcdbcd9a3dd7
Alexey K <afireinside47@gmail.com>
2016-02-01 21:01:03 +0300

    Change transaction.commit_on_success() to transaction.atomic()

--- allauth/account/models.py
+++ allauth/account/models.py
@@ -65,7 +65,7 @@ class EmailAddress(models.Model):
         """
         Given a new email address, change self and re-confirm.
         """
-        with transaction.commit_on_success():
+        with transaction.atomic():
             user_email(self.user, new_email)
             self.user.save()
             self.email = new_email
